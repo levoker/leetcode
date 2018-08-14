00cf823b815c87976508ffd7347870914cfeda8c
Leo Li <Leo.hyli@gmail.com>
2018-08-14 16:12:20 -0400

    Update 1twoSum.py

--- array/1twoSum.py
+++ array/1twoSum.py
@@ -1,1 +1,16 @@
-
+# 1. Two Sum
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        dict = {}
+        
+        for i, num in enumerate(nums):
+            if target - num in dict:
+                return(i, dict[target - num])
+            else:
+                dict[num] = i
