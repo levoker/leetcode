943f769a1f4603e2250e40cceb5bdbf018f28c3b
Leo Li <Leo.hyli@gmail.com>
2018-08-14 23:31:31 -0400

    Update 27.java

--- array/27.java
+++ array/27.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int begin = 0, end = nums.length -1;
+        while (begin <= end) {
+            if (nums[begin] == val) {
+                nums[begin] = nums[end];
+                end--;
+            } else {
+                begin++;
+            } 
+        }    
+        return end + 1;
+    }
+}
