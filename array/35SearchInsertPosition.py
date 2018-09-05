1cce414548139bdb4f75396014698250beb271ff
Leo Li <Leo.hyli@gmail.com>
2018-09-04 23:46:46 -0400

    Create 35SearchInsertPosition.py

---
+++ array/35SearchInsertPosition.py
@@ -0,0 +1,13 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        n = len(nums)
+        for i in range(0, n):
+            if nums[i] >= target:
+                return i
+        return n
+        
